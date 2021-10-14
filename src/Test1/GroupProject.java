ef614d7d37b5858fcafc4b935a8ec9b193ef0997
Altan-Ochir <altanochir0130@yahoo.com>
2021-10-13 22:48:11 -0500

    added int variable

--- src/Test1/GroupProject.java
+++ src/Test1/GroupProject.java
@@ -3,4 +3,8 @@ package Test1;
 public class GroupProject {
     ///asdklaslkdlsakdskllsnmdasm,nds,nm
     // group project here
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+    }
 }
